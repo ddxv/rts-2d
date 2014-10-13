1d15771a6f6a0857c03af0a6db69a5e5e333f7df
James O <burnthisemailplz@gmail.com>
2014-10-13 22:23:12 +0800

    rotation for ships fixed
    ships must face up in order for rotations to look right

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Unit : MonoBehaviour {
@@ -24,33 +24,17 @@ public class Unit : MonoBehaviour {
 						float z = 0;
 						Vector3 destination = new Vector3 (x, y, z);
 						StartMove (destination);
+
+			//This is currently working, by itself, to rotate ships, problem is they are not pointing correctly (sideways)
+						Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+						transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
 				}
 	  
-
-
-		//not sure if base.Update() is soemthing we need, from stormtek.geek.nz/rts_tutorial
-		//base.Update();
 		if(rotating) TurnToTarget();
 		else if(moving) MakeMove();
+			}
 
 
-	}
-
-
-//	public void OnMouseDown(GameObject hitObject, Vector3 hitPoint) {
-//		Debug.Log ("onmousedown");
-//		//base.MouseClick(hitObject, hitPoint, controller);
-//		//only handle input if owned by a human player and currently selected
-//		//if(player && player.human && currentlySelected) {
-//		//	if(hitObject.name == "Ground" && hitPoint != ResourceManager.InvalidPosition) {
-//				float x = hitPoint.x;
-//				float y = hitPoint.y;
-//				float z = 0;
-//				Vector3 destination = new Vector3(x, y, z);
-//				StartMove(destination);
-//		//	}
-//		//}
-//	}
 
 	public void StartMove(Vector3 destination) {
 		Debug.Log ("startmove started");
@@ -64,18 +48,8 @@ public class Unit : MonoBehaviour {
 	
 	
 	private void TurnToTarget() {
-		Debug.Log ("turntotarget");
-		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
-		Debug.Log ("turntotarget2");
-		//sometimes it gets stuck exactly 180 degrees out in the calculation and does nothing, this check fixes that
-		Quaternion inverseTargetRotation = new Quaternion(-targetRotation.x, -targetRotation.y, -targetRotation.z, -targetRotation.w);
 
-		if(transform.rotation == targetRotation || transform.rotation == inverseTargetRotation) {
-			//rotating = false;
-			//moving = true;
-			Debug.Log ("turntotarget3");
-		}
-		//moved these here, but should be in if statement above, having trouble getting rotation to work
+		//make sure ship rotates first
 		rotating = false;
 		moving = true;
 	}
@@ -85,7 +59,6 @@ public class Unit : MonoBehaviour {
 
 	private void MakeMove() {
 		transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * 3);
-		//rigidbody2D.AddForce(destination * 3 * Time.deltaTime);
 		Debug.Log ("makemove's transforming");
 		if(transform.position == destination) moving = false;
 	}
