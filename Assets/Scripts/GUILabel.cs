82ac7c6e79779cfa8c99dbf5ff873c6101cbf66b
James O <burnthisemailplz@gmail.com>
2014-11-04 13:57:16 +0800

    Added Files, nothing actually changed

---
+++ Assets/Scripts/GUILabel.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUILabel : MonoBehaviour {
+	
+	//change color
+	//spawn at specific point
+	//pass in a string to be displayed
+	
+	
+	private UILabel _lbl;
+	
+	public GameObject target;
+	public Camera worldCamera;
+	public Camera guiCamera;
+	
+	
+	
+	
+	void Awake () { 
+		_lbl = GetComponent<UILabel>();
+		//if (_lbl == null)
+		//	Debug.LogError ("CouldNOtodododdo");
+	}
+	
+	void Update () {
+		int score = Score.shipsDestroyed;
+		_lbl.text = "Ships Blasted:\n" + score.ToString ();
+	}
+	
+	
+	public Color TextColor {
+		get { return _lbl.color; }
+		set { _lbl.color = value; }
+		
+		
+	}
+	
+	public string Text {
+		get { return _lbl.text; }
+		set { _lbl.text = value; }
+	}
+	
+	
+	
+	
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+	
+	public static bool paused = false;
+	public Transform menu;
+	public Transform resume;
+	
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			paused = togglePause();
+			OnPaused();
+		}
+	}
+	
+	public void OnPaused()
+	{
+		if(paused)
+		{
+			//Instantiate(menu);
+			//Instantiate(resume);
+		}
+		
+	}
+	
+	
+	public bool togglePause()
+	{
+		if(Time.timeScale == 0f)
+		{
+			Time.timeScale = 1f;
+			//Destroy(GameObject.FindWithTag ("resume"));
+			//Destroy(GameObject.FindWithTag ("menu"));
+			
+			
+			return(false);
+		}
+		else
+		{
+			Time.timeScale = 0f;
+			return(true);   
+		}
+	}
+}
